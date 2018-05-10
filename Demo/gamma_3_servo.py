9d696a3658cd23f9580117b80d285a967b15d7ba
Ritwik Das <ritwikdas54@gmail.com>
2018-05-10 14:01:51 -0400

    Made CMU-MOSEI Branch

--- Demo/gamma_3_servo.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:8bc1fc781807e8de05fe00a34a6250e93de522b1b1ea703c73c4a4fcac087548
-size 3992
--- Demo/gamma_3_servo_multi.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:a61d5e8e8507753d50e0030edf3c0cde4b99a55a7db6d232bd191ae485692da4
-size 3430
