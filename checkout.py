0328bb25b8c067770203117dbdcd79d4cd13d650
Ritwik Das <ritwikdas54@gmail.com>
2018-05-11 01:22:41 -0400

    Added Vision Pickle Files

--- checkout.py
+++ checkout.py
@@ -1,15 +1,46 @@
-# import pickle
-# import pprint
-# import random
-# with open('covarep.pkl', 'rb') as f:
-# 	data = pickle.load(f)
-# pp = pprint.PrettyPrinter(indent=4)
-# # pp.pprint(data[0])
+import cPickle as pickle
+import pprint
+import random
+import os
+import numpy as np
 
-# for key,value in data.items():
-# 	pp.pprint(key)
-# 	for key2,value2 in value.items():
-# 		pp.pprint(key2)
-# 		# pp.pprint(value2)
-# 	break
+with open('facet.pkl', 'rb') as f:
+	data = pickle.load(f)
+pp = pprint.PrettyPrinter(indent=4)
+# pp.pprint(data[0])
+# pp.pprint(len(data))
+with open('train.pkl', 'rb') as f:
+	train_data = pickle.load(f)
+with open('valid.pkl', 'rb') as f:
+	valid_data = pickle.load(f)
+with open('test.pkl', 'rb') as f:
+	test_data = pickle.load(f)
+
+
+for key,value in data.items():
+	pp.pprint(key)
+	for key2,value2 in value.items():
+		if key2 in train_data:
+			folder_location = "./Multimodal_Emotion_Analysis/vision_files/test/"
+		elif key2 in valid_data:
+			folder_location = "./Multimodal_Emotion_Analysis/vision_files/train/"
+		else:
+			folder_location = "./Multimodal_Emotion_Analysis/vision_files/val/"
+		video_name = key2
+		for key3,value3 in value2.items():
+			segment_id  = key3
+			pickle_file = folder_location + video_name + '_' + segment_id + '.pkl'
+			print(pickle_file)
+			# print(value3[0][2])
+			for idx,frame in enumerate(value3):
+				if idx == 0:
+					facet_features = frame[2]
+				else:
+					facet_features = np.vstack((facet_features,frame[2]))
+			# pp.pprint(facet_features)
+			pickle.dump(facet_features, open(pickle_file, "wb" ))
+			# break
+		# break
+	# break
+
 
