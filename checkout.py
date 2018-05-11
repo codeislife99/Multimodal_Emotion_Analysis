4f899a3b720370e3cbbd2db48020c9e8ff9f5e83
Ritwik Das <ritwikdas54@gmail.com>
2018-05-10 22:51:10 -0400

    Added Multimodal Data

---
+++ checkout.py
@@ -0,0 +1,15 @@
+# import pickle
+# import pprint
+# import random
+# with open('covarep.pkl', 'rb') as f:
+# 	data = pickle.load(f)
+# pp = pprint.PrettyPrinter(indent=4)
+# # pp.pprint(data[0])
+
+# for key,value in data.items():
+# 	pp.pprint(key)
+# 	for key2,value2 in value.items():
+# 		pp.pprint(key2)
+# 		# pp.pprint(value2)
+# 	break
+
---
+++ mmdata/__init__.py
@@ -0,0 +1,4 @@
+__author__ = "MultiComp"
+
+from .dataset import Dataset
+from .dataloader import MOSI, MOSEI, Dataloader
