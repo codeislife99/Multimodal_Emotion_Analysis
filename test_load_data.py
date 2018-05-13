d81922cb682ff1111d4665543dee33be6423c344
Ritwik Das <ritwikdas54@gmail.com>
2018-05-13 01:52:47 -0400

    load data' ;

--- test_load_data.py
+++ test_load_data.py
@@ -1,3 +1,3 @@
 from utils_modified import *
 
-obj = MultimodalDataset(self, dataset, visual='facet', audio='covarep', text='embeddings', pivot='words', sentiments=False, emotions=True, max_len=20)
+obj = MultimodalDataset('MOSEI', visual='facet', audio='covarep', text='embeddings', pivot='words', sentiments=False, emotions=True, max_len=20)
