559777a61188bd39b758796185e2cc1030fd32db
Ritwik Das <ritwikdas54@gmail.com>
2018-05-13 01:50:04 -0400

    Testing Load Data

---
+++ test_load_data.py
@@ -0,0 +1,3 @@
+from utils_modified import *
+
+obj = MultimodalDataset(self, dataset, visual='facet', audio='covarep', text='embeddings', pivot='words', sentiments=False, emotions=True, max_len=20)
