f3d1190befe157819979935de94798c5b298d071
Shivang Bansal <shivangbansal1995@gmail.com>
2018-05-13 23:43:37 +0530

    Update transfer_valid_audio_multiprocessing.py

--- transfer_valid_audio_multiprocessing.py
+++ transfer_valid_audio_multiprocessing.py
@@ -1,98 +1,41 @@
-import file_walker
+import cPickle as pickle
 import os
-import cv2
-import numpy as np
-import imutils
-import shutil
-import random
-import time
+import glob
 
 from multiprocessing.pool import Pool
 from multiprocessing import cpu_count
 
-src_dir = "./Processed Data/data/sample_large/train/"
-dest_dir = "./Processed Data/data/sample_small/train/"
+def transefer_audio_files(split):
 
-SEED = time.time()
-
-def crawl(dir):
-	print(dir)
-	dst = dest_dir + dir + '/'
-	dir = src_dir + dir
-
-	video_dict = dict()
-
-	cnt = 0;
-	for f in file_walker.walk(dir):
-		video_no = int(f.name[0:4])
-		video_dict[video_no] = video_dict.get(video_no, []) + [f.full_path]
-		
-		# if ((frame_no % 6) == 0):
-		# 	shutil.copy(f.full_path, dst)
-		# 	cnt += 1
-		
-
-	for key, value in video_dict.items():
-		random.seed(SEED)
-		random.shuffle(value)
-		print(key, ":\n")
-
-		for val in value[0:8]:
-			shutil.copy(val, dst)
-			print(val)
-
-		print("\n")
-
-		# print(key, value)
-		# print(key, len(value))
-		
-		cnt+= 1
-		if cnt == 25:
-			break
-
-
-
-	# print(dir, "=>", cnt)
-
-	# when running without multi-processing 
-
-	"""
-	for f in file_walker.walk(dir):
-		if(f.isDirectory):
-			emotion_dir = f.name
-			dst = dest_dir + emotion_dir + '/'
-			print(emotion_dir, ":")
-
-			for sub_f in file_walker.walk(f.full_path):
-				frame_no = int(sub_f.name[22:])
-
-				if (frame_no >= 20 and frame_no <= 50):
-					shutil.copy(sub_f.full_path, dst)
-					cnt += 1
-	"""
-
-
-	return cnt
-
-
-if __name__ == '__main__':
-
-	pool = Pool(cpu_count()*2)
-	dir_to_crawl = ['A/', 'H/', 'N/', 'D/', 'S/', 'F/']
-
-	# dir_to_crawl = ['A/']
+	path = './audio_files/'+split+'/*.pkl'
 	count = 0
-	try:
-		for cnt in pool.imap_unordered(crawl, dir_to_crawl):
-			count += cnt
-			print (cnt)
+	for file in glob.glob(path):
+		with open(file,'rb') as f:
+			data = pickle.load(f)
+			# print(data.shape[1])
+			if data.shape[1] == 74:
+				os.system("mv "+file+" ./audio_files_74/"+split+"/")
+				# print("mv "+file+" ./audio_files_74/"+split+"/")
+			count += 1
+	print(split, count)
+	return count
 
-	except Exception as e:
-		print("exception:",e)
-		pool.close()
-		pool.terminate()
 
-	print(count)
+pool = Pool(cpu_count()*2)
 
-	
+splits = ['train','test','val']
+
+for split in splits
+	os.system("mkdir -p audio_files_74/"+split)
+
+count = 0
+try:
+	for cnt in pool.imap_unordered(transefer_audio_files, splits):
+		count += cnt
+		print (cnt)
+
+except Exception as e:
+	print("exception:",e)
+	pool.close()
+	pool.terminate()
 
