a6ed7ff9045406c0be8b8c6fbc2afa45dfe63857
Ritwik Das <ritwikdas54@gmail.com>
2018-05-13 13:44:31 -0400

    Using 74 audio files only

---
+++ transfer_valid_audio.py
@@ -0,0 +1,16 @@
+import cPickle as pickle
+import os
+import glob
+splits = ['train','test','val']
+for split in splits:
+	path = './audio_files/'+split+'/*.pkl'
+	count = 0
+	for file in glob.glob(path):
+		with open(file,'rb') as f:
+			data=pickle.load(f)
+			# print(data.shape[1])
+			if data.shape[1] == 74:
+				os.system("mv "+file+" ./audio_files_74/"+split+"/")
+				# print("mv "+file+" ./audio_files_74/"+split+"/")
+			count += 1
+	print(split, count)
