01d579d8f248ae400a53c982a5c26f6003418ee9
codeislife99 <ritwikdas54@gmail.com>
2018-05-14 20:20:30 +0000

    Triple Attention Debug

--- triple_attention.py
+++ triple_attention.py
@@ -55,7 +55,7 @@ class WordvecNet(nn.Module):
 	def __init__(self,input_size,hidden_size,num_layers,dropout=0.2,bidirectional=True):
 		super(WordvecNet, self).__init__()
 		# self.rnn = nn.LSTM(input_size,hidden_size,num_layers,dropout,bidirectional,batch_first=True)
-		self.rnn = nn.LSTM(input_size,hidden_size,num_layers,dropout,bidirectional)
+		self.rnn = nn.LSTM(input_size,hidden_size,num_layers,dropout,bidirectional=bidirectional)
 
 	def forward(self,x):
 		"""
