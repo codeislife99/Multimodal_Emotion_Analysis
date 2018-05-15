94fc59675b9a11c5bdff0f12c78c40ce50cb52d4
Ritwik Das <ritwikdas54@gmail.com>
2018-05-15 03:58:30 -0400

    Random Baselines Network Added for ALL Loss Functions

---
+++ random_baselines.py
@@ -0,0 +1,104 @@
+from torch.autograd import Variable
+from torch.nn.parameter import Parameter
+import torch.nn.functional as F
+import torch.nn as nn
+import numpy as np
+import torch
+from torch.utils.data import Dataset, DataLoader
+from mosei_dataloader import mosei
+import os
+
+class Random_Net(nn.Module):
+	def __init__(self):
+		super(Random_Net, self).__init__()
+		self.x = Parameter(torch.FloatTensor([0,0,0,0,0,0]))
+
+	def forward(self):
+		return self.x
+'----------------------------------------------------------------------------------------------------------------------'
+Random = Random_Net()
+Random = Random.cuda()
+criterion = nn.MSELoss(size_average = False) # MSE
+criterion = nn.L1Loss(size_average = False) # MAE
+critetion = nn.SmoothL1Loss(size_average=False) # Huber Loss
+params =  list(Random.parameters())
+optimizer = torch.optim.Adam(params, lr = 0.1)
+num_workers = 0
+test_mode = False
+val_mode = False
+train_mode = True
+use_pretrained = False
+'----------------------------------------------------------------------------------------------------------------------'
+if train_mode:
+	train_dataset = mosei(mode= "train")
+	data_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                        batch_size=1,
+                                        shuffle=True,num_workers = num_workers)
+elif val_mode:
+	val_dataset = mosei(mode = "val")
+	data_loader = torch.utils.data.DataLoader(dataset=val_dataset,
+                                        batch_size=1,
+                                        shuffle=False,num_workers = num_workers)
+	no_of_epochs = 1
+else:
+	test_dataset = mosei(mode = "test")
+	data_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                        batch_size=1,
+                                        shuffle=False,num_workers = num_workers)
+'------------------------------------------Saving Intermediate Models--------------------------------------------------'
+
+def save_checkpoint(state, filename = 'MSE_Loss_sum'):
+	filename = filename + '.pth.tar'
+	os.system("mkdir -p random_models")
+	torch.save(state, './random_models/'+filename)
+'----------------------------------------------------------------------------------------------------------------------'
+
+# y = Variable(torch.FloatTensor([1,2,3,4,5,6])).cuda()
+epoch = 0
+no_of_epochs = 1000
+while epoch < no_of_epochs:
+
+	if use_pretrained:
+		# pretrained_file = './DAN/dual_attention_net_iter_8000_0.pth.tar'
+		pretrained_file = './random_models/MSE_Loss_sum.pth.tar'
+
+		checkpoint = torch.load(pretrained_file)
+		Random.load_state_dict(checkpoint['Random'])
+		use_pretrained = False
+		if train_mode:
+			epoch = checkpoint['epoch']+1
+			optimizer.load_state_dict(checkpoint['optimizer'])
+
+	running_loss = 0.0 
+	K = 0
+	for i,(vision,vocal,emb,gt) in enumerate(data_loader):
+		gt = Variable(gt.float()).cuda()
+		x = Random()
+		x = torch.clamp(x,0,3)
+		loss = criterion(x,gt)
+		if train_mode:
+			loss.backward()
+			optimizer.step()
+			optimizer.zero_grad()
+
+		running_loss += loss.data[0]
+		K+=1
+		average_loss = running_loss/K
+		if train_mode:			
+			print('Training -- Epoch [%d], Sample [%d], Average Loss: %.4f'
+			% (epoch+1, K, average_loss))
+		elif val_mode:
+			print('Validating -- Epoch [%d], Sample [%d], Average Loss: %.4f'
+			% (epoch+1, K, average_loss))
+		else:
+			print('Testing -- Epoch [%d], Sample [%d], Average Loss: %.4f'
+			 % (epoch+1, K, average_loss))
+	print(x)
+	if train_mode:
+		save_checkpoint({
+			'epoch': epoch,
+			'loss' : average_loss,
+			'Random' : Random.state_dict(),
+			'optimizer': optimizer.state_dict(),
+		})
+	epoch+=1
