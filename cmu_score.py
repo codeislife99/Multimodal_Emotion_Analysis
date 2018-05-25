696a5519cc471876faeddd0a9b6906c9afab8c6b
Raymond Ng <cuhk.raymond@gmail.com>
2018-05-25 18:31:22 +0000

    WA and F1 score (in progress)

--- cmu_score.py
+++ cmu_score.py
@@ -6,6 +6,8 @@ def ComputePerformance(ref,hyp):
     # hyp_local=hyp.data.cpu().numpy()
     ref_local=ref
     hyp_local=hyp
+    no_of_examples=np.shape(ref_local)[0]
+    no_of_classes=np.shape(ref_local)[1]
 
     # print(ref_local)
     # print(hyp_local)
@@ -15,6 +17,23 @@ def ComputePerformance(ref,hyp):
     hyp_binary=np.zeros(np.shape(hyp_local))
     hyp_binary[hyp_local >= 0.5]=1
 
+    ref_class_binary=np.zeros((no_of_classes,no_of_examples))
+    hyp_class_binary=np.zeros((no_of_classes,no_of_examples))
+    for i in range(0,no_of_classes):
+      ref_class_binary[i][ref_local[:,i] >= 0.5]=1
+      hyp_class_binary[i][hyp_local[:,i] >= 0.5]=1
+      TP=np.sum(np.logical_and(ref_class_binary[i]==1,hyp_class_binary[i]==1))
+      TN=np.sum(np.logical_and(ref_class_binary[i]==0,hyp_class_binary[i]==0))
+      FP=np.sum(np.logical_and(ref_class_binary[i]==0,hyp_class_binary[i]==1))
+      FN=np.sum(np.logical_and(ref_class_binary[i]==1,hyp_class_binary[i]==0))
+      
+ 
+      score['WA'][i] = f1
+      score['F1customised'][i] =
+      score['F1'][i] =   
+   
+ 
+
     # ref_flat=np.reshape(ref_local,(1,np.prod(np.shape(ref_local))))
     # hyp_flat=np.reshape(hyp_local,(1,np.prod(np.shape(hyp_local))))
    
@@ -23,6 +42,10 @@ def ComputePerformance(ref,hyp):
     score=dict()
     # print(accuracy_score(np.reshape(ref_binary, (1,np.prod(np.shape(ref_binary))))[0],np.reshape(hyp_binary, (1,np.prod(np.shape(hyp_binary))))[0]))
     score['binaryaccuracy'] = accuracy_score(np.reshape(ref_binary, (1,np.prod(np.shape(ref_binary))))[0],np.reshape(hyp_binary, (1,np.prod(np.shape(hyp_binary))))[0])
+   
+    
+
+
     # print(ref_flat)
     # print(hyp_flat)
     # score['MSE'] = ((ref_flat - hyp_flat) ** 2).mean(axis=0)
