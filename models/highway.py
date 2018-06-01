b714a9c29ead0fa0b561fb8a877bdf6b520e8c37
roryjbeard <roryjbeard@gmail.com>
2018-06-01 01:09:53 +0100

    outergated script added

--- models/highway.py
+++ models/highway.py
@@ -34,14 +34,14 @@ class GatedMemUpdate_B(nn.Module):
     Differs from GatedMemUpdate by allowing both t and c to control
     the gating behaviour. In general the size of the tensor to be gated
     will differ from the size of the tensor controlling the gating.
-    Further, the tensor to be routed through when the gate is 'on' is 
+    Further, the tensor to be routed through when the gate is 'on' is
     in general another external signal u (neither c nor g).
     So, in summary,
     - t is passed when gate is 'off'
     - both t and g control the gate, and
     - u is passed through when gate is 'on'
     """
-    def __init__(self, gate_out_size, gate_in_size):
+    def __init__(self, gate_in_size, gate_out_size):
 
         super(GatedMemUpdate_B, self).__init__()
 
